d5710af7e736fe07314cc8b1fd48f78dab221981
ivikiwi-1111 <90843000+ivikiwi-1111@users.noreply.github.com>
2022-04-02 12:26:14 +0300

    main.py
    - Created a new TeleBot (Bowl_Bot_test)
    - Added new command (/Note) - returns the last message written in the chat, which is not a command
    - Created a function for making notes (just sends the last message written in the chat yet)
    - Haven't done a container-cleaner yet (bad)

---
+++ main.py
@@ -0,0 +1,27 @@
+import telebot;
+bot = telebot.TeleBot('5146293499:AAGkPTPbwpECUaM00lPonkimKYsIbgoN9UU');
+
+p = [""]
+def CreateNote(last_message, id):
+    """
+    Realization
+    :param last_message:
+    :return: bot.send_message()
+    """
+    bot.send_message(id, "The last message is: " + last_message)
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "/help":
+        bot.send_message(message.from_user.id, "Available features:\n"
+                                               "/Note"
+                                               " - Creates a new note after call")
+    if message.text == "/Note":
+        CreateNote(p[-1], message.from_user.id)
+        #Clean Realization wasn't done yet - clean(p)
+    else:
+        last_message = message.text
+        p.append(last_message) #bad idea
+
+bot.polling(none_stop=True, interval=0)
